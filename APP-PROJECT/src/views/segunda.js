a36d816cb6187962a99ff50b66fcb9a81d6caac7
msanchezb18 <msanchezb@est.utn.ac.cr>
2025-04-21 14:53:16 -0600

    Examen2
    Examen2

---
+++ APP-PROJECT/src/views/segunda.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+import { style_segunda } from '../styles/style_segunda';
+import GameSetup from '../components/gameSetup.js'
+
+const Segunda = () => {
+
+  return (
+    <View style={style_segunda.divMain}>
+    <View style={style_segunda.contentContainer}>
+      <View style={style_segunda.encabezado}>
+        <Text style={style_segunda.title1}>TetraTilt</Text>
+      </View>
+    <GameSetup/>
+    </View>
+    <View style={style_segunda.divFooter}>
+      <Text style={style_segunda.textFooter}>
+        Bienvenidos al juego en el que tu cabeza tendrá que trabajar
+      </Text>
+    </View>
+  </View>
+  );
+};
+
+export default Segunda;
